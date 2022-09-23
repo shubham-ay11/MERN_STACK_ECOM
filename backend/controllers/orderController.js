fff5c86dcfe1aed1379fedb55d164fbe4ead45ff
shubham-ay11 <shubhjoshi114@gmail.com>
2022-09-23 18:21:58 +0530

    Created Order Get & Delete APi

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,5 @@
 const { json } = require("body-parser");
 
-const Product = require("../models/productModels");
 const ErrorHander = require("../utlis/errorhandler");
 const asyncErrorHandle = require("../middleware/catchAsyncError");
 const Apifeatures = require("../utlis/apifeatures");
@@ -18,7 +17,7 @@ exports.createOrder = asyncErrorHandle(async (req, res, next) => {
     totalPrice,
   } = req.body;
 
-  const order= await Order.create({
+  const order = await Order.create({
     shippingInfo,
     orderItems,
     paymentInfo,
@@ -26,12 +25,53 @@ exports.createOrder = asyncErrorHandle(async (req, res, next) => {
     taxprice,
     shippingPrice,
     totalPrice,
-    paideAt:Date.now(),
-    user:req.user._id,
+    paideAt: Date.now(),
+    user: req.user._id,
   });
 
   res.status(200).json({
-    sucess:true,
-    order
-  })
+    sucess: true,
+    order,
+  });
+});
+
+// Get All Order Details
+exports.getAllOrders = asyncErrorHandle(async (req, res, next) => {
+  const order = await Order.find();
+  if (!order) {
+    return next(new ErrorHander("Order not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// Get Specific Order
+exports.getSpecificOrder = asyncErrorHandle(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHander("Order not Found with this is", 404));
+  }
+
+  res.status(200).json({
+    sucess: true,
+    order,
+  });
+});
+
+// Delete Order
+exports.deleteOrder = asyncErrorHandle(async (req, res, next) => {
+  console.log("order");
+
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHander("Order Not found", 404));
+  }
+
+  await order.remove();
+  res.status(200).json({
+    success: true,
+  });
 });
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const {
-    createOrder,
- 
+  createOrder,
+  getAllOrders,
+  getSpecificOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 const { isAuthUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/order/new").post(isAuthUser,createOrder);
-
-
-
+router.route("/order/new").post(isAuthUser, createOrder);
+router.route("/orders").get(isAuthUser, getAllOrders);
+router.route("/order/:id").get(isAuthUser, getSpecificOrder);
+router.route("/delete_Order/:id").delete(isAuthUser, deleteOrder);
 
 module.exports = router;
