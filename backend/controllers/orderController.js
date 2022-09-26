9eab1c9ae068cb0affa077f7110cf981b549c5ae
shubham-ay11 <shubhjoshi114@gmail.com>
2022-09-26 16:50:44 +0530

    Logged in user order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,22 +35,31 @@ exports.createOrder = asyncErrorHandle(async (req, res, next) => {
   });
 });
 
-// Get All Order Details
+// Get All Order Details Admin
 exports.getAllOrders = asyncErrorHandle(async (req, res, next) => {
-  const order = await Order.find();
-  if (!order) {
+  const orders = await Order.find();
+  if (!orders) {
     return next(new ErrorHander("Order not found", 404));
   }
 
+  let totalAmount=0;
+
+  orders.forEach((order) =>{
+    totalAmount+=order.totalPrice;
+  });
+
   res.status(200).json({
     success: true,
-    order,
+    orders,
+    totalAmount 
   });
 });
 
 // Get Specific Order
 exports.getSpecificOrder = asyncErrorHandle(async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
+  
+  const order = await Order.findById(req.params.id).populate("user", "name email");
+  
   if (!order) {
     return next(new ErrorHander("Order not Found with this is", 404));
   }
@@ -60,6 +69,8 @@ exports.getSpecificOrder = asyncErrorHandle(async (req, res, next) => {
     order,
   });
 });
+
+
 
 // Delete Order
 exports.deleteOrder = asyncErrorHandle(async (req, res, next) => {
@@ -73,5 +84,20 @@ exports.deleteOrder = asyncErrorHandle(async (req, res, next) => {
   await order.remove();
   res.status(200).json({
     success: true,
+  });
+});
+
+// Get order of Logged user
+
+exports.myOrders = asyncErrorHandle(async (req,res,next) =>{
+
+console.log("herkujfreu");
+const orders = await Order.find({user: req.user._id});
+  
+  
+
+  res.status(200).json({
+    sucess: true,
+    orders,
   });
 });
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,13 +4,15 @@ const {
   getAllOrders,
   getSpecificOrder,
   deleteOrder,
+  myOrders
 } = require("../controllers/orderController");
 const { isAuthUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/order/new").post(isAuthUser, createOrder);
-router.route("/orders").get(isAuthUser, getAllOrders);
+router.route("/orders").get(isAuthUser, authorizeRoles("admin"), getAllOrders);
 router.route("/order/:id").get(isAuthUser, getSpecificOrder);
 router.route("/delete_Order/:id").delete(isAuthUser, deleteOrder);
+router.route("/orders/me").get(isAuthUser, myOrders);
 
 module.exports = router;
